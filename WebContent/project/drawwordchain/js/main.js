fb19d458a88d782377af378db9e0b7b0ad1c6839
AmatsukiUrato <AmatsukiUrato@yahoo.co.jp>
2017-01-11 16:57:21 +0900

    main.js/main.htmlの整理

--- WebContent/project/drawwordchain/js/main.js
+++ WebContent/project/drawwordchain/js/main.js
@@ -1,107 +1,51 @@
 var xmlHttpRequest;
 
-function sendSendRequest() {
-	var messageElement = document.getElementById("message");
-
-	var url = "send";
-	xmlHttpRequest = new XMLHttpRequest();
-	xmlHttpRequest.onreadystatechange = checkSendRequest;
-	xmlHttpRequest.open("POST", url, true);
-	xmlHttpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	xmlHttpRequest.send("message=" + messageElement.value);
-}
-
-function receiveSendResponse() {
-	//var response = eval("(" + xmlHttpRequest.responseText + ")");
-}
-
-function checkSendRequest() {
-	if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
-		receiveSendResponse();
-	}
-}
-
-function sendUpdateRequest() {
-	var url = "update";
-	xmlHttpRequest = new XMLHttpRequest();
-	xmlHttpRequest.onreadystatechange = checkUpdateRequest;
-	xmlHttpRequest.open("GET", url, true);
-	xmlHttpRequest.send(null);
-}
-
-function receiveUpdateResponse() {
-	//console.log(xmlHttpRequest.responseText);
-	//var response = eval("(" + xmlHttpRequest.responseText + ")");
-	var response = JSON.parse(xmlHttpRequest.responseText);
-
-	var nameElement = document.getElementById("name");
-	nameElement.innerHTML = response.user.name;
-
-	var statementListElement = document.getElementById("statement_list");
-	statementListElement.innerHTML = "";
-	for(var i = 0; i < response.statementList.length; i++) {
-		var statement = response.statementList[i];
-
-		var statementElement = document.createElement("tr");
-		statementListElement.appendChild(statementElement);
-
-		var userElement = document.createElement("td");
-		statementElement.appendChild(userElement);
-		userElement.innerHTML = statement.user.name + ">";
-
-		var messageElement = document.createElement("td");
-		statementElement.appendChild(messageElement);
-		messageElement.innerHTML = statement.message;
-	}
-}
-
-function checkUpdateRequest() {
-	if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
-		receiveUpdateResponse();
-	}
-}
-
 function checkStartRequest() {
-	// 開始ボタンのレスポンス処理部分
-	if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
-		receiveStartResponse();
-	}
+    // 開始ボタンのレスポンス処理部分
+    if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
+        receiveStartResponse();
+    }
 }
 
+// 返信データの処理
 function receiveStartResponse() {
-	var response = JSON.parse(xmlHttpRequest.responseText);
+    var response = JSON.parse(xmlHttpRequest.responseText);
 
-	var playerAreaElement = document.getElementById("player_area");
+    var playerAreaElement = document.getElementById("player_area");
 
-	while (playerAreaElement.lastChild) {
-	  playerAreaElement.removeChild(playerAreaElement.lastChild);
-	}
+    while (playerAreaElement.lastChild) {
+        playerAreaElement.removeChild(playerAreaElement.lastChild);
+    }
 
-	for(var i = 0; i < response.playerList.length; i++) {
-		var playerElement = document.createElement("span");
-		playerAreaElement.appendChild(playerElement);
-		playerElement.innerHTML = "player"+(i+1)+":"+response.playerList[i];
-		playerAreaElement.appendChild(document.createElement("br"));
-	}
+    for (var i = 0; i < response.playerList.length; i++) {
+        var playerElement = document.createElement("span");
+        playerAreaElement.appendChild(playerElement);
+        playerElement.innerHTML = "player" + (i + 1) + ":" + response.playerList[i];
+        playerAreaElement.appendChild(document.createElement("br"));
+    }
 }
 
+// ページ読み込み時
 window.addEventListener("load", function() {
+	// 開始ボタンのEventListener
+    var startButtonElement = document.getElementById("start_button");
+    startButtonElement.addEventListener("click", function() {
+        var url = "start";
+        xmlHttpRequest = new XMLHttpRequest();
+        xmlHttpRequest.onreadystatechange = checkStartRequest;
+        xmlHttpRequest.open("GET", url, true);
+        xmlHttpRequest.send(null);
+    }, false);
+
+	// 送信ボタンのEventListener
 	var sendButtonElement = document.getElementById("send_button");
-
 	sendButtonElement.addEventListener("click", function() {
-		var url = "start";
-		xmlHttpRequest = new XMLHttpRequest();
-		xmlHttpRequest.onreadystatechange = checkStartRequest;
-		xmlHttpRequest.open("GET", url, true);
-		xmlHttpRequest.send(null);
+		// 画像->Jsonデータに変換
+		var data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+		console.log(data);
+		console.log(JSON.stringify(data));
+		alert(JSON.stringify(data));
+		// 画像送信の記述なし
+
 	}, false);
-
-	// sendUpdateRequest();
-	//
-	// setInterval(sendUpdateRequest, 1000);
 }, false);
-
-window.addEventListener('beforeunload', function(event) {
-  console.log('I am the 1st one.');
-  // これでブラウザバック時にセッションを削除する．
-});
