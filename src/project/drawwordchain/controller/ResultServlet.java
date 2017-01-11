2514682b1dd615dd61f5d5d9258175ac0ca67a9d
AmatsukiUrato <AmatsukiUrato@yahoo.co.jp>
2017-01-11 15:38:07 +0900

    ResultServletにApplicationScopeにあるDrawingインスタンスの削除項目を追記

--- src/project/drawwordchain/controller/ResultServlet.java
+++ src/project/drawwordchain/controller/ResultServlet.java
@@ -56,6 +56,9 @@ public class ResultServlet extends ActionServlet {
 		PrintWriter writer = response.getWriter();
 		writer.append(json);
 		writer.flush();
+
+        // DrawingをapplicationScopeから削除
+        application.removeAttribute("drawing");
 	}
 
 }
