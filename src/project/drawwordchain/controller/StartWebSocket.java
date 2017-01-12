777212840cf8e215c7f338fd1e45453c5215a920
AmatsukiUrato <AmatsukiUrato@yahoo.co.jp>
2017-01-12 09:01:29 +0900

    StartWebSocketに@OnOpenのconnect/broadcastメソッドを作成

--- src/project/drawwordchain/controller/StartWebSocket.java
+++ src/project/drawwordchain/controller/StartWebSocket.java
@@ -2,17 +2,27 @@ package project.drawwordchain.controller;
 
 import java.io.IOException;
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/project/drawwordchain/startbroadcast")
-public class SampleWebSocket {
+public class StartWebSocket {
 
-    @OnMessage
-    public void broadcast(String message, Session session) throws IOException {
-        for ( Session s : session.getOpenSessions() ) {
-            s.getAsyncRemote().sendText(message);
+    private static final Queue<Session> sessions = new ConcurrentLinkedQueue<>();
+
+    @OnOpen
+    public void connect(Session session) {
+        sessions.add(session);
+        broadcast();
+    }
+
+    public void broadcast() {
+        for ( Session s : sessions ) {
+            s.getAsyncRemote().sendText("message");
         }
     }
 }
