60006d8df452970e481b437ab589765d030b327b
AmatsukiUrato <AmatsukiUrato@yahoo.co.jp>
2017-01-16 06:09:00 +0900

    Info.javaの追加

--- src/project/drawwordchain/model/Info.java
+++ src/project/drawwordchain/model/Info.java
@@ -1,7 +1,9 @@
 package project.drawwordchain.model;
 
+// json = {"playerName":"myName","imgName":"","img":""}
 public class Info {
-    public String name;
-    public String email;
+    public String playerName;
+    public String[] userList;
+    public String imgName;
     public String img;
 }
