be6c11606bbc4b6f3d9270046f43786e9307d805
AmatsukiUrato <AmatsukiUrato@yahoo.co.jp>
2017-01-17 22:43:45 +0900

    getLocation.jsの追加

---
+++ WebContent/project/drawwordchain/js/getLocation.js
@@ -0,0 +1,6 @@
+//URLを取得
+// http://localhost:8080/isp2/project/drawwordchain/main.html
+// ws://localhost:8080/isp2/project/drawwordchain/startbroadcast
+var tmp = "ws"+location.href.substr(4);
+var wsLocationResult = tmp.substr(0, tmp.length - 9);
+console.log(wsLocationResult);
